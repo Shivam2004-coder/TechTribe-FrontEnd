f63587a57e57475de6be8d76b9eaf5f606910dc9
Shivam2004-coder <shivamgit003@gmail.com>
2025-07-01 15:12:09 +0530

    updated the userAvatar image links

--- src/Components/Profile/EditOptions/Options/UserAvatar.jsx
+++ src/Components/Profile/EditOptions/Options/UserAvatar.jsx
@@ -27,7 +27,7 @@ const UserAvatar = () => {
                       <div className="w-10 rounded-full">
                         {/* <img src="/boyAvatar1.jpg" alt="BoyAvatar1" /> */}
                         <div className="w-full h-full object-cover rounded-lg" >
-                            <AdvancedImage cldImg={cld.image("TechTribe_User_Profile_Avatar/User_Avatars/Profile_avatar_a83fe293-40ae-458d-8423-83bfec78dbbb").resize(fill().width(250).height(250))} />
+                            <AdvancedImage cldImg={cld.image("boyAvatar1_k6nddv").resize(fill().width(250).height(250))} />
                         </div>
                       </div>
                   </div>
@@ -35,7 +35,7 @@ const UserAvatar = () => {
                       <div className="w-10 rounded-full">
                         {/* <img src="/boyAvatar2.jpg" alt="BoyAvatar2" /> */}
                         <div className="w-full h-full object-cover rounded-lg" >
-                            <AdvancedImage cldImg={cld.image("TechTribe_User_Profile_Avatar/User_Avatars/Profile_avatar_424fc5f1-b325-4461-bac7-749391c70640").resize(fill().width(250).height(250))} />
+                            <AdvancedImage cldImg={cld.image("boyAvatar2_ilignp").resize(fill().width(250).height(250))} />
                         </div>
                       </div>
                   </div>
@@ -43,7 +43,7 @@ const UserAvatar = () => {
                       <div className="w-10 rounded-full">
                       {/* <img src="/boyAvatar3.jpg" alt="BoyAvatar3" /> */}
                       <div className="w-full h-full object-cover rounded-lg" >
-                          <AdvancedImage cldImg={cld.image("TechTribe_User_Profile_Avatar/User_Avatars/Profile_avatar_b833471e-8f1b-4fc7-82b0-caec4f8f7fee").resize(fill().width(250).height(250))} />
+                          <AdvancedImage cldImg={cld.image("boyAvatar3_ghehfe").resize(fill().width(250).height(250))} />
                       </div>
                       </div>
                   </div>
